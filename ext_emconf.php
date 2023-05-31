6496cdda9bb152c5fd3872f9b1c5503777e37179
Christian Dilger <c.dilger@addorange.de>
2023-05-31 10:05:29 +0200

    [CHANGE] update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,16 @@ $EM_CONF[$_EXTKEY] = [
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '9.5.0',
+	'version' => '8.7.0',
 	'constraints' => [
 		'depends' => [
             'typo3' => '7.6.0-8.7.99',
+            'rkw_basics' => '8.7.80-8.7.99',
+            'rkw_events' => '8.7.108-8.7.199',
+            'rkw_mailer' => '9.5.12-9.5.99',
+            'rkw_registration' => '8.7.41-8.7.99',
+            'rkw_shop' => '8.7.26-8.7.99',
+            'rkw_survey' => '8.7.69-8.7.99'
 		],
 		'conflicts' => [
 		],
