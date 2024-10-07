0c1b97d20117f0ebb2b51c0b362b56b871f7b4f9
Christian Dilger <c.dilger@addorange.de>
2024-10-07 14:25:06 +0200

    [CHANGE] update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,7 +28,7 @@ $EM_CONF[$_EXTKEY] = [
             'accelerator' => '10.4.0-12.4.99',
             'fe_register' => '10.4.0-12.4.99',
             'postmaster' => '10.4.0-12.4.99',
-            'rkw_events' => '9.5.0-10.4.99',
+            'rkw_events' => '10.4.0-12.4.99',
             'rkw_shop' => '10.4.0-12.4.99',
             'rkw_survey' => '10.4.0-12.4.99'
 		],
